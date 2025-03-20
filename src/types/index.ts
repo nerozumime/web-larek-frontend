7a2f800bb23dadec11f8c4cce1a9dc335d7a385a
Daniel Lunev <lunev.danik@gmail.com>
2025-03-20 16:37:40 +0300

    init

---
+++ src/types/index.ts
@@ -0,0 +1,62 @@
+type ItemCategory = 'софт-скил' | 'другое' | 'дополнительное' | 'кнопка' | 'хард-скил'
+type ItemTitle = string;
+type ItemImage = string;
+type ItemDescription = string;
+type ItemPrice = number | string;
+type ItemId = number;
+
+interface IItem {
+  category: ItemCategory;
+  title: ItemTitle;
+  image: ItemImage;
+  price: ItemPrice;
+  description: ItemDescription;
+  id: ItemId;
+}
+
+type ButtonElement = HTMLButtonElement;
+
+interface IItemInBasket {
+  itemPosition: number;
+  itemContent: IItem;
+  removeButton: ButtonElement;
+}
+
+type TotalPrice = number | null;
+
+interface IBasket {
+  items: IItemInBasket[];
+  totalPrice: TotalPrice;
+}
+
+type PaymentMethod = 'Онлайн' | 'При получении'
+type Email = string;
+type PhoneNumber = string;
+
+interface IOrder {
+  paymentMethod: PaymentMethod;
+  shipAdress: string;
+  email: Email;
+  phoneNumber: PhoneNumber;
+}
+
+type InputElement = HTMLInputElement;
+
+interface IAppStateManager {
+  items: IItem[];
+  basket: IBasket;
+  order: IOrder;
+
+
+  initItems(items: IItem[]): void;
+  
+  addItemToBasket(id: ItemId): void;
+  removeItemFromBasket(id: ItemId) : void;
+  getBasketItemsCount(): number;
+  makeOrder(): void;
+  getTotalPrice(): TotalPrice;
+  clearBasket(): void;
+
+  setPaymentMethod(paymentMethod: PaymentMethod): void;
+  setOrderInputValue(input: InputElement): void;
+}
